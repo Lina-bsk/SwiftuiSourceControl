6cf59d2677be501aff45ef31b5857b2ea8761164
Lina <zlina.bsk@gmail.com>
2025-04-01 18:22:30 +0200

    Add HomeView

---
+++ SwiftuiSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftuiSourceControl
+//
+//  Created by Lina Zhabska on 4/1/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
