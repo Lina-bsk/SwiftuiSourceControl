263e4b31fbc3541a5293e38994fe06e16a81e742
Lina <zlina.bsk@gmail.com>
2025-04-01 12:25:53 +0200

    Initial Commit

---
+++ SwiftuiSourceControl/SwiftuiSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiSourceControlApp.swift
+//  SwiftuiSourceControl
+//
+//  Created by Lina Zhabska on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
