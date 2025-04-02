a917ada8583aed9e566b813ebc5dbda259844b97
Lina <zlina.bsk@gmail.com>
2025-04-02 21:12:19 +0200

    [Patch] fix text

--- SwiftuiSourceControl/ContentView.swift
+++ SwiftuiSourceControl/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello world!")
+            Text("Hello world")
             Text("Hello world!")
             Text("Text in a new branch!")
             Button("Subscribe on this page") {
