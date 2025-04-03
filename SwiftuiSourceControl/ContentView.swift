93ff8142c0478503c88e870ebde86a3155499128
Lina <zlina.bsk@gmail.com>
2025-04-03 14:31:09 +0200

    second commit

--- SwiftuiSourceControl/ContentView.swift
+++ SwiftuiSourceControl/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack(spacing: 5) {
             ScrollView {
                 ForEach(0..<5) { i in
-                    Text("\(i + 1) number of houses!")
+                    Text("\(i + 1) numbers of houses!")
                         .padding()
                     Image(systemName: "house.fill")
                         .foregroundStyle(Color.indigo)
