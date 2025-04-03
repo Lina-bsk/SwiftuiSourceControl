709533fe9f57efa0d7dd5570c2e6fc2f9755d857
Lina-bsk <39274589+Lina-bsk@users.noreply.github.com>
2025-04-03 14:39:37 +0200

    Add push notifications (#2)
    * commit
    * second commit

--- SwiftuiSourceControl/ContentView.swift
+++ SwiftuiSourceControl/ContentView.swift
@@ -12,12 +12,12 @@ struct ContentView: View {
         VStack(spacing: 5) {
             ScrollView {
                 ForEach(0..<5) { i in
-                    Text("\(i + 1) number of houses!")
+                    Text("\(i + 1) numbers of houses!")
                         .padding()
                     Image(systemName: "house.fill")
                         .foregroundStyle(Color.indigo)
                     
-                    Button("Press") {
+                    Button("Pre") {
                         
                     }
                 }
