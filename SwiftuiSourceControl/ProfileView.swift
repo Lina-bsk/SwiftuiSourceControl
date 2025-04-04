7a46ff04da9df5146433d6800f4d373174ff4e6c
Lina-bsk <39274589+Lina-bsk@users.noreply.github.com>
2025-04-04 20:35:53 +0200

    Feature/abs 123 (#4)
    * Feature/add profile (#3)
    
    * [Feature] Create profile
    
    * [Feature] add description
    
    * file
    
    * [Feature] add text

---
+++ SwiftuiSourceControl/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  SwiftuiSourceControl
+//
+//  Created by Lina Zhabska on 4/4/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Your profile")
+            .padding()
+        Text("Your info")
+        Image(systemName: "star.fill")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
