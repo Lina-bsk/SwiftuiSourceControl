3e54b6b02fafee23e4e2c9fe907d3532d9b24ac5
Lina <zlina.bsk@gmail.com>
2025-04-04 20:03:31 +0200

    [Feature] Create profile

---
+++ ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftuiSourceControl
+//
+//  Created by Lina Zhabska on 4/4/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Your profile")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
